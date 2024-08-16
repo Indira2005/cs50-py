c69daf52d01929a32646d6fe7baf8b761ab7ab18
Indira Patil <indiraprashantpatil@gmail.com>
2024-08-16 23:16:24 +0530

    add solution for pset1 - deep thought

---
+++ ps1/deep.py
@@ -0,0 +1,5 @@
+ans = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+if ans == "42" or ans.lower() == "forty-two" or ans.lower() == "forty two":
+    print("Yes")
+else :
+    print("No")
